4f581a99a2dbc05c032afe15d6f129d6a0f81ce9
nari-z <go.to18ishi@gmail.com>
2020-05-03 18:57:39 +0900

    add static image

--- presenter/host/router.go
+++ presenter/host/router.go
@@ -1,6 +1,10 @@
 package host
 
 import (
+	"os"
+
+	"github.com/go-openapi/runtime/middleware"
+
 	"github.com/nari-z/drunk-api/bundler"
 
 	"github.com/nari-z/drunk-api/generate/restapi/operations"
@@ -16,7 +20,18 @@ func NewRouter(api *operations.DrunkAPI, h *bundler.HahdleBundler) {
 	api.AddLiquorHandler = operations.AddLiquorHandlerFunc(h.LiquorHandler.RegistLiquor)
 	api.GetLiquorsHandler = operations.GetLiquorsHandlerFunc(h.LiquorHandler.GetLiquorList)
 
-	// TODO: add static file root(/LiquorImage)
+	// add static file root(/LiquorImage)
 	// // TODO: ディレクトリの設定を全体と共有したい。
-	// e.Static("/LiquorImage", "LiquorImage")
+	api.GetLiquorImageFileNameHandler = operations.GetLiquorImageFileNameHandlerFunc(func(params operations.GetLiquorImageFileNameParams) middleware.Responder {
+		// return middleware.NotImplemented("operation GetLiquorImageFileName has not yet been implemented")
+		// TODO: 暫定。ディレクトリ追加するだけで可能としたい。
+		//       like a `e.Static("/LiquorImage", "LiquorImage")`
+
+		file, err := os.Open("./LiquorImage/" + params.FileName)
+		if err != nil {
+			return operations.NewGetLiquorImageFileNameBadRequest()
+		}
+
+		return operations.NewGetLiquorImageFileNameOK().WithPayload(file)
+	})
 }
